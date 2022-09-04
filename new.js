6fc2f7cf0c108795dbddeb0166e062904345723b
Mikhail Novikov <elqver@gmail.com>
2022-09-04 13:34:01 +0300

    arraymethodsresults+rollD20

--- new.js
+++ new.js
@@ -1,12 +1,13 @@
-countChars = function(string, char) {
-    counter = 0;
-    for (a = string.length; a > 0; a--) {
-        inspectedChar = string.charAt(a - 1);
-        if (inspectedChar == char.toUpperCase() || inspectedChar == char.toLowerCase()){
-            counter++;
-        }
-    }
-    return counter;
-}
-
-console.log(countChars('beekeeper', 'E'));
+var array = [1, 2, 3, 5];
+console.log(array);
+console.log(array.pop()); // возвращает элемент, изменяет массив
+console.log(array);
+console.log(array.push(7)); // возвращает какую-то хуйню, изменяет массив
+console.log(array);
+console.log(array.shift()); // возвращает элемент, изменяет массив
+console.log(array);
+console.log(array.unshift(0)); // возвращает какую-то хуйню, изменяет массив
+console.log(array);
+console.log(array.slice(1, 3)); // возвращает новый массив, не изменяет старый массив
+console.log(array);
+console.log(Math.round(Math.random() * 20)); // roll d20
