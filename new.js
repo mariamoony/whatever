d4bec7ee78e36a0ab746858dcbf73f1adac6d966
Mikhail Novikov <elqver@gmail.com>
2022-09-04 15:58:08 +0300

    sumrangetaskfinal

--- new.js
+++ new.js
@@ -1,13 +1,14 @@
 function range(first, last, step) {
     arRange = [];
+    if (step == undefined) step = 1;
     if (first <= last) {
-        for (i = first; i <= last; (step == undefined ? i++ : i += step))
+        for (i = first; i <= last; i += step)
             arRange.push(i);
         return arRange;
     }
-    for (i = -first; i <= -last; (step == undefined ? i++ : i -= step))
-        arRange.push(-i);
-    return arRange; 
+    for (i = first; i >= last; i += step)
+        arRange.push(i);
+    return arRange;
 
 }
 
@@ -15,8 +16,9 @@ console.log(range(5, 2, -2));
 
 function sum(array) {
     result = 0;
-    for (i in array)
+    for (i in array) {
         result += array[i];
+    }
     return result;
 }
 
