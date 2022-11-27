dbdf3cbc0f142d8f9d761fad7c356c90e71a3b75
Mikhail Novikov <elqver@gmail.com>
2022-11-27 23:01:38 +0300

    reducearraystask

--- new.js
+++ new.js
@@ -1,25 +1,12 @@
-String.prototype.format = function () {
-    // store arguments in an array
-    var args = arguments;
-    // use replace to iterate over the string
-    // select the match and check if the related argument is present
-    // if yes, replace the match with the argument
-    return this.replace(/{([0-9]+)}/g, function (match, index) {
-      // check if the argument is present
-      return typeof args[index] == 'undefined' ? match : args[index];
-    });
-};
-
-function politeFunc(f, txt) {
-    return function(arg) {
-        res = f(arg);
-        console.log(txt.format(arg, res));
+function reduceArrays(array) {
+  return array.reduce(
+    function f(current, next) {
+      newCurrent = current.concat(next);
+      return newCurrent;
     }
+  )
 }
 
-function tempConvertFtoC(valNum) {
-    return (valNum-32) / 1.8;
-}
+var arrays = [[1, 2, 3], [4, 5], [6]];
 
-politeConvert = politeFunc(tempConvertFtoC, '{0} Fahrenheit = {1} Celsius');
-politeConvert(446);
+console.log(reduceArrays(arrays));
